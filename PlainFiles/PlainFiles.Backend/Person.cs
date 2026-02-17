99856b2de24c3fbe848b9e7f719692111ab019ad
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 14:49:46 -0500

    Create class Person

---
+++ PlainFiles/PlainFiles.Backend/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace PlainFiles.Backend;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
