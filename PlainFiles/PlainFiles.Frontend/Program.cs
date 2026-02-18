8a3b673fffd93c4f388bf44b3e2d775df67fafd0
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-18 18:55:39 -0500

    Updates

--- PlainFiles/PlainFiles.Frontend/Program.cs
+++ PlainFiles/PlainFiles.Frontend/Program.cs
@@ -1,12 +1,11 @@
 ﻿using PlainFiles.Backend;
 using System.ComponentModel.Design;
 
-var textFile = new SimpleTextFile(".\\Data\\data.txt");
-var lines = textFile.ReadAllLines().ToList();
+Console.Write("Enter the list name: ");
+var listName = Console.ReadLine();
+var manualCsv = new ManualCsvHelper();
+var people = manualCsv.ReadCsv($"{listName}.csv");
 var opt = string.Empty;
-
-using var logger = new LogWriter(".\\app.log");
-logger.WriteLog("INFO", "Application started");
 
 do
 {
@@ -14,62 +13,134 @@ do
     switch (opt)
     {
         case "1":
-            Console.WriteLine("Showing file content: ");
-            logger.WriteLog("INFO", "The contents of the file were displayed");
-            foreach (var line in lines)
-            {
-                Console.WriteLine(line);
-            }
+            Console.Write("Enter name: ");
+            var name = Console.ReadLine() ?? string.Empty;
+            Console.Write("Enter lastName: ");
+            var lastName = Console.ReadLine() ?? string.Empty;
+            Console.Write("Enter age: ");
+            var age = Console.ReadLine() ?? string.Empty;
+            people.Add(new[] { name, lastName, age });
+            Console.WriteLine("Person added.");
             break;
 
         case "2":
-            Console.Write("Enter a new line to add: ");
-            var newLine = Console.ReadLine() ?? string.Empty;
-            if (string.IsNullOrWhiteSpace(newLine))
+            Console.WriteLine($"List of {listName}");
+            Console.WriteLine("Showing list of people: ");
+            Console.WriteLine(" --------------------------------------------------------");
+            Console.WriteLine("|\tName\t|\tLast Name\t|\tAge\t|");
+            Console.WriteLine(" --------------------------------------------------------");
+            foreach (var person in people)
             {
-                Console.WriteLine("Line cannot be empty. Please try again");
-                logger.WriteLog("WARNING", "Nothing was added");
-                break;
+                Console.WriteLine($"|   {person[0]}\t|\t{person[1]}\t|\t{person[2]}\t|");
+                Console.WriteLine(" --------------------------------------------------------");
             }
-            lines.Add(newLine);
-            Console.WriteLine("Line added");
-            logger.WriteLog("INFO", $"Added: {newLine}");
             break;
 
         case "3":
-            textFile.WriteAllLines(lines.ToArray());
-            Console.WriteLine("File saved");
-            logger.WriteLog("INFO", "The file was saved");
+            saveFile(people, listName);
+            Console.WriteLine("List saved.");
             break;
 
         case "0":
             Console.WriteLine("Exiting...");
-            logger.WriteLog("INFO", "Application finished");
             break;
 
         default:
-            Console.WriteLine("Invalid option. Please try again");
-            logger.WriteLog("ERROR", "An invalid option was selected");
+            Console.WriteLine("Invalid option. Please try again.");
             break;
     }
 }
 while (opt != "0");
-textFile.WriteAllLines(lines.ToArray());
-Console.WriteLine("File saved");
-logger.WriteLog("INFO", "The file was saved");
+saveFile(people, listName);
 
 string Menu()
 {
-    Console.WriteLine("1. Show");
-    Console.WriteLine("2. Add");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Show");
     Console.WriteLine("3. Save");
     Console.WriteLine("0. Exit");
     Console.Write("Choose an option: ");
     return Console.ReadLine() ?? string.Empty;
 }
 
+void saveFile(List<string[]> people, string? listName)
+{
+    manualCsv.WriteCsv($"{listName}.csv", people);
+}
+
 //////////////////////////////////////////////////////
-/// CLASS SIMPLETEXTFIEL EXAMPLE WITH USER INPULT ////
+////// CLASS LOGWRITER EXAMPLE WITH USER INPULT //////
+//////////////////////////////////////////////////////
+
+//var textFile = new SimpleTextFile(".\\Data\\data.txt");
+//var lines = textFile.ReadAllLines().ToList();
+//var opt = string.Empty;
+
+//using var logger = new LogWriter(".\\app.log");
+//logger.WriteLog("INFO", "Application started");
+
+//do
+//{
+//    opt = Menu();
+//    switch (opt)
+//    {
+//        case "1":
+//            Console.WriteLine("Showing file content: ");
+//            logger.WriteLog("INFO", "The contents of the file were displayed");
+//            foreach (var line in lines)
+//            {
+//                Console.WriteLine(line);
+//            }
+//            break;
+
+//        case "2":
+//            Console.Write("Enter a new line to add: ");
+//            var newLine = Console.ReadLine() ?? string.Empty;
+//            if (string.IsNullOrWhiteSpace(newLine))
+//            {
+//                Console.WriteLine("Line cannot be empty. Please try again");
+//                logger.WriteLog("WARNING", "Nothing was added");
+//                break;
+//            }
+//            lines.Add(newLine);
+//            Console.WriteLine("Line added");
+//            logger.WriteLog("INFO", $"Added: {newLine}");
+//            break;
+
+//        case "3":
+//            textFile.WriteAllLines(lines.ToArray());
+//            Console.WriteLine("File saved");
+//            logger.WriteLog("INFO", "The file was saved");
+//            break;
+
+//        case "0":
+//            Console.WriteLine("Exiting...");
+//            logger.WriteLog("INFO", "Application finished");
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid option. Please try again");
+//            logger.WriteLog("ERROR", "An invalid option was selected");
+//            break;
+//    }
+//}
+//while (opt != "0");
+//textFile.WriteAllLines(lines.ToArray());
+//Console.WriteLine("File saved");
+//logger.WriteLog("INFO", "The file was saved");
+
+//string Menu()
+//{
+//    Console.WriteLine("1. Show");
+//    Console.WriteLine("2. Add");
+//    Console.WriteLine("3. Save");
+//    Console.WriteLine("0. Exit");
+//    Console.Write("Choose an option: ");
+//    return Console.ReadLine() ?? string.Empty;
+//}
+
+//////////////////////////////////////////////////////
+/// CLASS SIMPLETEXTFIEL EXAMPLE WITH USER INPUT /////
 //////////////////////////////////////////////////////
 
 //var textFile = new SimpleTextFile(".//data//data.txt");
