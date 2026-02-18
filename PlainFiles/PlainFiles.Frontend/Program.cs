4e4ff7a34f42fe7eb97cb86059b86765488f867d
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-18 14:03:55 -0500

    Updates

---
+++ PlainFiles/PlainFiles.Frontend/Program.cs
@@ -0,0 +1,17 @@
+﻿using PlainFiles.Backend;
+
+var textFile = new SimpleTextFile("data.txt");
+var random = new Random();
+var lines = new List<string>();
+for (int i = 0; i < 1000; i++)
+{
+    var number = random.Next(1, 1000000);
+    lines.Add($"Random number: {number:N0}");
+}
+textFile.WriteAllLines(lines.ToArray());
+
+var linesReaded = textFile.ReadAllLines();
+foreach (var line in linesReaded)
+{
+    Console.WriteLine(lines);
+}
